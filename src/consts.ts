9db40263f7af609f8d4752c3b3457434365f22d0
Jakub Szwajka <szwajkajakub@gmail.com>
2026-04-21 09:36:41 +0200

    Refine site branding and hide Lucy pages

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'KUBA SZWAJKA // ARCHITECT';
-export const SITE_DESCRIPTION = 'Software Architect based in Wrocław. Focusing on EdTech, FinTech, and modular architecture.';
+export const SITE_TITLE = 'KUBA SZWAJKA';
+export const SITE_DESCRIPTION = 'Doing Software. Based in Wrocław.';
 
 // Social links
 export const GITHUB_URL = 'https://github.com/JakubSzwajka';
